bcbf788f25158dbdea94b7ba35458170089fa979
Hannah Young <hannah@corgibyte.com>
2022-07-13 16:06:21 -0700

    Change login strategy

--- mybot.mjs
+++ mybot.mjs
@@ -10,31 +10,34 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const login = async () => {
+  if (!client.isReady) {
+    await client.login(process.env.DISCORD_TOKEN);
+  }
+};
+
 // get guild
 const getGuild = async (guildID) => {
-  await client.login(process.env.DISCORD_TOKEN);
+  await login();
   const guild = await client.guilds.fetch(guildID);
-  client.destroy();
   return guild; // returns the promise of a discord guild
 };
 
 // get channel
 const getChannel = async (channelID) => {
-  await client.login(process.env.DISCORD_TOKEN);
+  await login();
   const channel = await client.channels.fetch(channelID);
-  client.destroy();
   return channel; // returns the promise of a discord channel
 };
 
 // create webhook
 const createWebhook = async (channelID) => {
-  await client.login(process.env.DISCORD_TOKEN);
+  await login();
   const channel = await client.channels.fetch(channelID);
   const webhook = await channel.createWebhook("hook", {
     avatar: "https://i.imgur.com/wSTFkRM.png",
   });
   webhook.send("webhook has been created!");
-  client.destroy();
   return webhook.url;
 };
 
@@ -44,6 +47,7 @@ const run = async () => {
   const channel = await getChannel("964211195982794765");
   const webhook = await createWebhook("964211195982794765");
   console.log(`Guild: ${guild}\nChannel: ${channel}\nWebhook: ${webhook}`);
+  client.destroy();
 };
 
 run();
